eb4cae3d6c060e102c8f3d44461226905819295a
ReAlMs710 <neil.kumaran@mcvts.org>
2025-03-12 11:58:36 -0400

    added main menu

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cstdlib> // For system()
-#include <algorithm> // For shuffle
-#include <random> // For random device
+#include <string>
 
 using namespace std;
 
+string usb = "No Drive Selected"; 
+
 void clearScreen() {
-    cout << "\033[2J\033[H"; // ANSI escape code to clear screen
+    cout << "\033[2J\033[H" << flush; // ANSI escape code to clear screen
 }
 
 bool GPTFDiskInstallCheck() {
@@ -22,7 +23,7 @@ void installGptfdisk() {
     if (response == 'y' || response == 'Y') {
         system("sudo pacman -S gptfdisk");
     } else {
-        cerr << "gptfdisk is required for ";
+        cerr << "gptfdisk is required for creating GPT partition schemes!";
         exit(1);
     }
 }
@@ -30,30 +31,36 @@ void installGptfdisk() {
 void selectUSBDevice() {
     while (true) { 
         clearScreen();
-        string usb = "No USB Drive Selected";
         string usbInitialSelection; 
         char selection;
-        cout << "Please select a USB Device from the list (ensure that it is the flash drive)\n\n";
+        cout << "Please select the drive from the list (ensure that it is the flash drive)\n\n";
         system("sudo fdisk -l | grep -E '^Disk /dev/[a-z]+|^Disk /dev/nvme[0-9]+'");
         cout << "\nPlease type the identifier of the desired USB Flash Drive (ex. /dev/sda, /dev/sdb)\n";
         cin >> usbInitialSelection;
-
         clearScreen();
+
         system(("DEVICE=\"" + usbInitialSelection + "\"; DEVNAME=$(basename \"$DEVICE\"); "
                 "[ -b \"$DEVICE\" ] && echo \"Size: $(lsblk -no SIZE \"$DEVICE\" | head -n 1) | "
                 "Model: $(lsblk -no MODEL \"$DEVICE\") | "
                 "Removable: $(cat /sys/class/block/$DEVNAME/removable)\" || "
                 "echo \"Device $DEVICE does not exist.\"").c_str());
 
-        cout << "\nDoes this look right? (y/n)";
+        cout << "\nDoes this look right? (y/n)\n";
         cin >> selection;
         
         if (selection == 'y') {
-            usb = usbInitialSelection;
-            break;
+            cout << "\nAre you sure? This drive may be WIPED later. (y/n)\n";
+            cin >> selection;
+            if (selection == 'y') {
+                usb = usbInitialSelection;
+                break;
+            }
+            else {
+
+            }
         }
         else {
-            cout << "Redo";
+
         }
 
     }
@@ -70,6 +77,34 @@ void partToGPT() {
     }
 }
 
-int main () {
-    selectUSBDevice(); 
+void menu() {
+    while (true) {
+        int selection;
+        clearScreen();
+        cout << "FlashBurn\n\n";
+        if (usb == "No Drive Selected") {
+            cout << "Selected Flash Drive: \033[31m" + usb + "\033[0m\n\n";  // Red if no drive selected
+        } else {
+            cout << "Selected Flash Drive: \033[34m" + usb + "\033[0m\n\n";  // Blue otherwise
+        }
+
+        cout << "1. Select a flash drive\n";
+        cin >> selection;
+
+        switch (selection) {
+            case 1:
+                selectUSBDevice();
+                break;
+            default:
+                cout << "Wrong selection";
+        }
+    }
+}
+
+int main() {
+    if (!GPTFDiskInstallCheck()) {
+        installGptfdisk;
+    }
+    menu();
+    return 0;
 }
